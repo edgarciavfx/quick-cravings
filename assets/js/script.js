afe258e6aefdb2a6e256c0cfaed7a4e10d50fb91
edgarciavfx <edgarciavfx@gmail.com>
2025-11-26 16:20:03 -0600

    add style.css for card-grid

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// DOM 
+// DOM Elements
 const devUtil = document.querySelector('#dev');
 const searchForm = document.querySelector('#search-form');
 const searchInput = document.querySelector('#search-input');
@@ -19,15 +19,20 @@ const fetchRecipes = async (ingredient) => {
 const displayRecipes = recipes => {
     recipesContainer.innerHTML = ''; // Clear previous results
     
+    if (!recipes) {
+        recipesContainer.innerHTML = '<p class="text-muted">No recipes found. Try another ingredient!</p>';
+        return;
+    }
+
     recipes.forEach(recipe => {
         const recipeCard = `
-    <div class="card h-100 shadow-sm">
-        <img src="${recipe.strMealThumb}" class="card-img-top w-100" alt="${recipe.strMeal}">
-        <div class="card-body">
-            <h5 class="card-title">${recipe.strMeal}</h5>
-            <a href="#" class="btn btn-primary">View Recipe</a>
-        </div>
-    </div>`;
+            <div id="${recipe.idMeal}" class="card recipe-card h-100 border-0 shadow-sm">
+              <img src="${recipe.strMealThumb}" class="card-img-top" alt="${recipe.strMeal}">
+              <div class="card-body d-flex flex-column">
+                <h5 class="card-title">${recipe.strMeal}</h5>
+                <a href="#" class="btn btn-primary mt-auto">View Recipe</a>
+              </div>
+            </div>`;
         
         recipesContainer.innerHTML += recipeCard;
     });
